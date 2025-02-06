fe62b01cc33dc36b9f87720f4f9b91d4b9a22fab
karthikeyamadhavan123 <karthikeyadonnipad2005@gmail.com>
2025-02-06 20:08:51 +0530

    fifth commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default async function RootLayout({
           <LoginModal/>
         <Navbar currentUser = {currentUser}/>
         </ClientOnly>
-        <div className="pb-20 pt-28">
+        <div className="pb-20 pt-28 pl-4">
         {children}
         </div>
         
